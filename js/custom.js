d77f3bd27f378ff91ad22940c81e968c14002bae
User-09 <cl.beamon@yahoo.com>
2016-08-29 20:07:03 -0700

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+// $(function() {
+//   $(".dropdown").delay(1000).fadeIn("slow");
+// });
+// $('#close-ad').click(function() {
+//   $('.dropdown').fadeOut("slow");
+// });
+// $('.thumbnail').hover(function(){
+//   $('#text-content').fadeToggle("slow");
+// });
